76c5f9e2ecbb40dec16938628803aaf89e825050
bryantmuhammad <Bryantmuhammad00@gmail.com>
2022-08-25 22:36:46 +0700

    Memperbaiki informasi footer dan menambahkan file ketentuan dari midtrans

--- resources/views/customer/layouts/footer.blade.php
+++ resources/views/customer/layouts/footer.blade.php
@@ -1,66 +1,30 @@
     <!-- Footer Section Begin -->
     <footer class="footer-section spad">
         <div class="container">
-            <div class="newslatter-form">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <form action="#">
-                            <input type="text" placeholder="Your email address">
-                            <button type="submit">Subscribe to our newsletter</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+
             <div class="footer-widget">
                 <div class="row">
                     <div class="col-lg-3 col-sm-6">
                         <div class="single-footer-widget">
-                            <h4>About us</h4>
+                            <h4>Tentang Kami</h4>
                             <ul>
-                                <li>About Us</li>
-                                <li>Community</li>
-                                <li>Jobs</li>
-                                <li>Shipping</li>
-                                <li>Contact Us</li>
+                                <li>Bakpia 716 Annur</li>
+                                <li>+62 812 8234 923</li>
+                                <li>Jl. Purwodiningratan NG1/ no 716 DIY Yogyakarta</li>
+
                             </ul>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-sm-6">
+                    <div class="col-lg-4 col-sm-6">
                         <div class="single-footer-widget">
-                            <h4>Customer Care</h4>
+                            <h4>Syarat Dan Ketentuan</h4>
                             <ul>
-                                <li>Search</li>
-                                <li>Privacy Policy</li>
-                                <li>2019 Lookbook</li>
-                                <li>Shipping & Delivery</li>
-                                <li>Gallery</li>
+                                <li><a href="/syarat.pdf" style="color:#fff;">Syarat Dan Ketentuan</a></li>
+
                             </ul>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="single-footer-widget">
-                            <h4>Our Services</h4>
-                            <ul>
-                                <li>Free Shipping</li>
-                                <li>Free Returnes</li>
-                                <li>Our Franchising</li>
-                                <li>Terms and conditions</li>
-                                <li>Privacy Policy</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="single-footer-widget">
-                            <h4>Information</h4>
-                            <ul>
-                                <li>Payment methods</li>
-                                <li>Times and shipping costs</li>
-                                <li>Product Returns</li>
-                                <li>Shipping methods</li>
-                                <li>Conformity of the products</li>
-                            </ul>
-                        </div>
-                    </div>
+
                 </div>
             </div>
         </div>
@@ -83,8 +47,7 @@
                     <script>
                         document.write(new Date().getFullYear());
                     </script> All rights reserved | This template is made with <i
-                        class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com"
-                        target="_blank">Colorlib</a>
+                        class="icon-heart color-danger" aria-hidden="true"></i> by Bakpia 716 Annur</a>
                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 </p>
             </div>
